7eaa508ddb66eb7e31f9f29b8326f8c52d6867a5
interisti <nika.interisti@gmail.com>
2018-06-15 20:29:19 +0400

    implement before deploy hook to check and create if deployment bucket does not exists

--- index.js
+++ index.js
@@ -7,9 +7,55 @@ class CreateDeploymentBucketPlugin {
     this.provider = this.serverless.getProvider('aws')
 
     this.hooks = {
+      'before:deploy:deploy': this.beforeDeploy.bind(this)
     }
   }
 
+  beforeDeploy() {
+    const bucketName = this.getBucketName()
+    if (!bucketName) {
+      this.serverless.cli.log(
+        'No custom deployment bucket is set, nothing to create'
+      )
+
+      return Promise.resolve()
+    }
+
+    return this.bucketExists(bucketName)
+      .then(exists => {
+        if (exists) {
+          this.serverless.cli.log(
+            `"${bucketName}" bucket already exists, nothing to create`
+          )
+
+          return Promise.resolve()
+        }
+
+        return this.createBucket(bucketName)
+          .then(() =>
+            this.serverless.cli.log(`"${bucketName}" bucket was created`)
+          )
+      })
+  }
+
+  getBucketName() {
+    return this.serverless
+      .service
+      .provider
+      .deploymentBucket
+  }
+
+  bucketExists(bucketName) {
+    return this.provider
+      .request('S3', 'headBucket', { Bucket: bucketName })
+      .then(() => true)
+      .catch(() => false)
+  }
+
+  createBucket(bucketName) {
+    return this.provider
+      .request('S3', 'createBucket', { Bucket: bucketName })
+  }
 }
 
 module.exports = CreateDeploymentBucketPlugin
